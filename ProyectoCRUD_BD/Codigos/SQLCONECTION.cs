332cea09b9cdcf67d9291121afdd7626a4b298d4
raul567 <raulpvtopro@gmail.com>
2025-11-21 03:19:04 -0600

    conexiones agregadas

--- ProyectoCRUD_BD/Codigos/SQLCONECTION.cs
+++ ProyectoCRUD_BD/Codigos/SQLCONECTION.cs
@@ -7,10 +7,16 @@ namespace ProyectoCRUD_BD.BDSQL
         private static string connectionString =
     "Server=localhost;Database=BDAsistenciaTecnica;Trusted_Connection=True;Encrypt=True;TrustServerCertificate=True;";
 
+
+        private static string connectionString2 =
+"Server=DESKTOP-GNLT0BG\\MSSQLSERVER01;Database=Centro_Tecnico_Electronico;Trusted_Connection=True;Encrypt=True;TrustServerCertificate=True;";
         // METODO PARA USAR EN TODAS LAS TABS
         public static SqlConnection GetConnection()
         {
             return new SqlConnection(connectionString);
+
+            return new SqlConnection(connectionString2);
+            
         }
     }
 }
